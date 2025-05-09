4d12c12867528a4801e905d4b4e3a180fc378c6b
Will <meltonjw3@gmail.com>
2025-05-09 14:59:03 -0500

    added prefixes for video uploads

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,17 +1,61 @@
 package main
 
 import (
+	"bytes"
+	_"math"
+	"crypto/rand"
 	"fmt"
 	"io"
+	"log"
 	"mime"
 	"net/http"
 	"os"
-	_"github.com/aws/aws-sdk-go-v2/config"
+	"os/exec"
+	"encoding/json"
+	_ "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
-	"crypto/rand"
 )
+
+type ratio struct {
+	Streams []struct {
+	Height int  `json:"height"`
+	Width	 int  `json:"width"`
+} `json:"streams"`
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams",filePath)
+	var buffer bytes.Buffer
+	cmd.Stdout =  &buffer
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("command finihsed with error: %v",err)
+		return "",err
+	}
+
+	ratio := ratio{}
+
+	err = json.Unmarshal(buffer.Bytes(),&ratio)
+	if err != nil {
+		log.Printf("error: %v",err)
+		return "",err
+	}
+	ratioVal := float64(ratio.Streams[0].Width) / float64(ratio.Streams[0].Height)
+
+if ratioVal > 1.6 && ratioVal < 1.9 {
+    return "landscape", nil
+// Portrait check (9:16 is approximately 0.56)
+} else if ratioVal > 0.5 && ratioVal < 0.6 {
+    return "portrait", nil
+} else {
+    return "other", nil
+}
+
+
+}
 
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
 
@@ -43,7 +87,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	if video.UserID != userID {
+	if video.UserID != userID && err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Video does not belong to authenticated user", err)
 		return
 	}
@@ -84,6 +128,9 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+
+
+
 	defer os.Remove(tmpfile.Name())
 	defer tmpfile.Close()
 
@@ -93,11 +140,16 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	prefix, err := getVideoAspectRatio(tmpfile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get aspect ratio",err)
+		return
+	}
 	tmpfile.Seek(0, io.SeekStart)	
 
 	randomByte := make([]byte, 16)
 	rand.Read(randomByte)
-	key := fmt.Sprintf("%x.mp4",randomByte)
+	key := fmt.Sprintf("%s/%x.mp4",prefix,randomByte)
 
 	s3Struct := s3.PutObjectInput{Bucket: &cfg.s3Bucket,
 							Key:  &key,
